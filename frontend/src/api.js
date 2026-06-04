d81f47dbe94448b184ff7535124e8c680e3e9249
dj-oussama <djoussama206@gmail.com>
2026-06-04 18:09:06 +0100

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000',
+    baseURL: import.meta.env.VITE_API_URL || 'https://prz-5ose.onrender.com/',
 });
 
 api.interceptors.request.use(
